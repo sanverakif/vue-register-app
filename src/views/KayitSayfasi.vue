b611db4f287d45bc2a96743b69b931f721e4d18a
Akif Emre Şanver <sanveremreakif@gmail.com>
2022-05-30 02:22:01 +0300

    add form component- submit work..

--- src/views/KayitSayfasi.vue
+++ src/views/KayitSayfasi.vue
@@ -1,68 +1,65 @@
 <template>
-  <div class="mainDiv">
-    <br /><br />
-    <div><img class="center" src="../assets/userIcon.png" /></div>
-    <v-container>
-      <v-row justify="space-between">
-        <v-col>
-          <!--form içerisinde bulunan datalar tetiklenme ile kayıt altına alınır.-->
-          <v-form ref="form" id="forms">
-            <!--değer girişleri için gereken inputlar-->
-            <v-text-field
-              label="Kullanıcı Adı"
-              placeholder="abcd123"
-              id="kullaniciAdi"
-              v-model="kullaniciAdi"
-            ></v-text-field>
+  <v-form>
+    <div class="mainDiv">
+      <br /><br />
+      <div><img class="center" src="../assets/userIcon.png" /></div>
+      <v-container>
+        <v-row justify="space-between">
+          <v-col>
+            <!--form içerisinde bulunan datalar tetiklenme ile kayıt altına alınır.-->
+            <v-form ref="form" id="forms">
+              <!--değer girişleri için gereken inputlar-->
+              <v-text-field
+                label="Kullanıcı Adı"
+                placeholder="abcd123"
+                id="kullaniciAdi"
+                v-model="kullaniciAdi"
+              ></v-text-field>
 
-            <!--İnputa girişin sağlanması için belirtilen şekilde girilmelidir ve bunun geçerli olması için ise required değerini kullanmamız gerekmektedir
-            
-            
+              <!--İnputa girişin sağlanması için belirtilen şekilde girilmelidir ve bunun geçerli olması için ise required değerini kullanmamız gerekmektedir
             -->
+              <v-text-field
+                v-model="mail"
+                label="Mail"
+                placeholder="sa@gmail.com"
+                id="mail"
+                :rules="mailRules"
+                required
+              >
+              </v-text-field>
 
-            <v-text-field
-              v-model="mail"
-              label="Mail"
-              placeholder="sa@gmail.com"
-              id="mail"
-              :rules="mailRules"
-              required
-            >
-            </v-text-field>
-
-            <!--sifre aynı olmalı-karakter kontrolu- 1 büyük 1 küçük dahil en az 8 karakter-şifre güçlüğü belirlenicek.-->
-            <v-text-field label="Şifre" placeholder="Ankara" v-model="sifre">
-            </v-text-field>
-            <v-text-field
-              label="Şifre Tekrar"
-              placeholder="Ankara"
-              v-model="sifreTekrar"
-            ></v-text-field>
-            <v-col>
-              <td style="float: left">
-                <!--Karakter sınırı: girilen değer 2.tekrarda aynı olmalıdır-->
-                <v-slider
-                  label="Max characters"
-                  style="width: 200px"
-                ></v-slider>
-              </td>
-              <td style="float: right">
-                <!--şartlar sağlanırsa kayıt olunup başarılı mesajı gösterilsin.-->
-                <v-btn
-                  style="background-color: lightblue; float: right"
-                  @click="passWordLogin"
-                  >Kayıt ol</v-btn
-                >
-              </td>
-               
-            </v-col>
-          
-          </v-form>
-        </v-col>
-      </v-row>
-       <v-col>{{ sifreSonuc }}</v-col>
-    </v-container>
-  </div>
+              <!--sifre aynı olmalı-karakter kontrolu- 1 büyük 1 küçük dahil en az 8 karakter-şifre güçlüğü belirlenicek.-->
+              <v-text-field label="Şifre" placeholder="Ankara" v-model="sifre">
+              </v-text-field>
+              <v-text-field
+                label="Şifre Tekrar"
+                placeholder="Ankara"
+                v-model="sifreTekrar"
+              ></v-text-field>
+              <v-col>
+                <td style="float: left">
+                  <!--Karakter sınırı: girilen değer 2.tekrarda aynı olmalıdır-->
+                  <v-slider
+                    label="Max characters"
+                    style="width: 200px"
+                  ></v-slider>
+                </td>
+                <td style="float: right">
+                  <!--şartlar sağlanırsa kayıt olunup başarılı mesajı gösterilsin.-->
+                  <v-btn
+                    style="background-color: lightblue; float: right"
+                    @click="passWordLogin"
+                    >Kayıt ol</v-btn
+                  >
+                </td>
+              </v-col>
+            </v-form>
+          </v-col>
+        </v-row>
+        <v-col>{{ sifreSonuc }}</v-col>
+      </v-container>
+    </div>
+  </v-form>
 </template>
 
 <script>
@@ -130,12 +127,12 @@ export default {
         return false;
       }
     },
+
     reset() {
       /*getelementby id ve ref : document içerisindeki bir elemente veya idye ulaşmak istersek bunu $ref kullanarak da ulaşabiliriz
       asağıda iste bir kosul belirtmeden form içerisindeki istenilen elementlerin değerlirini temizledik*/
       this.$refs.form.reset();
     },
-
     // clearForm() {
     //   this.kullaniciAdi = "";
     //   this.sifre = "";
